64986373cfe10685d00587bff64a9e4fa5a6913e
Hamir Mahal <hamirmahal@gmail.com>
2023-05-16 14:40:30 -0700

    chore: init project with TypeScript and `eslint`

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+// https://typescript-eslint.io/getting-started/#step-2-configuration
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true
+};
